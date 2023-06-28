23786af40e38b7a3a83a1e071daab3afbb17c069
ccy <yustart@foxmail.com>
2023-06-28 22:29:44 +0800

    update code

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 
+
 # ESP32-VFD-V1
 > VFD fluorescent screen multifunctional WIFI clock made with esp32-c3
 > 用esp32-c3制作的VFD荧光屏多功能WIFI时钟
 
-## 解锁GPIO11后芯片被我搞坏了，不出意外应该是软砖了。TM的ESP32时钟项目就到此截止了
--- src/main.cpp
+++ src/main.cpp
@@ -2,48 +2,48 @@
  * @Description:
  * @Author: chenzedeng
  * @Date: 2023-04-02 12:40:12
- * @LastEditTime: 2023-04-04 22:44:01
+ * @LastEditTime: 2023-06-28 22:26:20
  */
 #include <Arduino.h>
+#include <FastLED.h>
 #include <WiFi.h>
-#include <ds1302.h>
 #include <vfd.h>
 
-const char* ssid = "输入你的WIFI名称";
-const char* password = "输入你的WIFI密码";
+#define NUM_LEDS 1
+#define DATA_PIN GPIO_NUM_0
+
+const char* ssid = "Connect-YC";
+const char* password = "13083278337";
 
 #define NTP1 "ntp1.aliyun.com"
 #define NTP2 "ntp2.aliyun.com"
 #define NTP3 "ntp3.aliyun.com"
 
+CRGB leds[NUM_LEDS];
+
 void setClock();
 
 void setup() {
     Serial.begin(115200);
-    //初始化GPIO是输出模式
+    // 初始化GPIO是输出模式
     pinMode(GPIO_NUM_6, OUTPUT);
     pinMode(GPIO_NUM_4, OUTPUT);
     pinMode(GPIO_NUM_3, OUTPUT);
     pinMode(GPIO_NUM_5, OUTPUT);
     pinMode(GPIO_NUM_1, OUTPUT);
-    // pinMode(LED, OUTPUT);
+    pinMode(DATA_PIN, OUTPUT);
 
-    //设置PWM的频率单位hz
-    analogWriteFrequency(10000);
-
-    //初始化VFD
-    VFD_Init();
-    VFD_Clear();
-    VFD_ClearAD();
-    VFD_SetString(0, "Connecting..");
-    VFD_SetDisplayOnOff(DISPLAY_ON);
+    FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
 
     // WIFI Setting
     WiFi.begin(ssid, password);
-
+    uint8_t hue = 0;
     while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
         Serial.print(".");
+        // let's set an led value
+        hue += 10;
+        leds[0] = CHSV(hue, 255, 255);
+        FastLED.delay(33);
     }
 
     Serial.println("");
@@ -51,8 +51,16 @@ void setup() {
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
 
+    // 设置PWM的频率单位hz
+    analogWriteFrequency(15000);
+    // 初始化VFD
+    VFD_Init();
+    VFD_Clear();
+    VFD_ClearAD();
+    VFD_SetString(0, "Initializing");
+    VFD_SetDisplayOnOff(DISPLAY_ON);
+
     configTime(8 * 3600, 0, NTP1, NTP2, NTP3);
-    // put your setup code here, to run once:
 }
 
 void loop() {
@@ -79,16 +87,15 @@ void setClock() {
     // Serial.print(asctime(&timeinfo));  // 默认打印格式：Mon Oct 25 11:13:29
     // 2021
 
-    String datetime = String(timeinfo.tm_mon + 1);
-    datetime += "/";
-    datetime += timeinfo.tm_mday;  // 日
-    datetime += " ";
+    String datetime = String(timeinfo.tm_mday);
+    // datetime += timeinfo.tm_mday;  // 日
+    datetime += "D ";
     datetime += timeinfo.tm_hour;  // 时
     datetime += ":";
     datetime += timeinfo.tm_min;
     datetime += ":";
     datetime += timeinfo.tm_sec;
-    //发送到VFD显示
+    // 发送到VFD显示
     VFD_Clear();
     VFD_SetString(0, datetime.c_str());
 }
